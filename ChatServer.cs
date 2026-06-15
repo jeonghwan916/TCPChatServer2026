fb0a1950667616e2af2056dda9ffafc96a9ac3c1
Lee JaeHyun <myleje@gmail.com>
2026-06-15 10:59:58 +0900

    feat: ChatServer 기본 로직 구현

---
+++ ChatServer.cs
@@ -0,0 +1,41 @@
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPChatServer;
+
+public class ChatServer
+{
+    // TCP 프로토콜을 사용해서 클라이언트가 접속하기를 대기하는 클래스 : Listener Server
+    // 소켓 (Socket)
+    private TcpListener? _listener;
+    
+    // 서버 실행 여부
+    private bool _isRunning;
+    
+    // 서버 포트
+    private int _port;
+    
+    // 생성자 (Constructor)
+    public ChatServer(int port)
+    {
+        _port = port;
+        _isRunning = false;
+    }
+    
+    // 서버 시작
+    public void StartServer()
+    {
+        if (_isRunning)
+        {
+            Console.WriteLine("서버가 이미 실행 중 입니다.");
+            return;
+        }
+        
+        // TCP 소켓을 OPEN : TCP 리스너를 초기화 한다.
+        _listener = new TcpListener(IPAddress.Any, _port);
+        _listener.Start();
+        _isRunning = true;
+        
+        Console.WriteLine($"서버가 정상적으로 실행되었습니다. 포트:{_port}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿namespace ChatServer;
+﻿namespace TCPChatServer;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Windows 콘솔에서 UTF-8 인코딩 설정
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        
+        // 기본 포트 번호 설정 0 ~ 65536
+        // 1000번 포트 이하는 미리 예약 http 80, ssh 21, ...
+        const int port = 7777;
+        
+        // 채팅 서버 인스턴스 생성 및 실행
+        var server = new ChatServer(port);
+        // 서버 시작
+        server.StartServer();
+        
+        Console.WriteLine("서버를 종료하려면 아무키나 입력하세요.");
+        Console.ReadKey(true); // 입력한 키 값을 Hook해서 화면에 출력되지 않도록 하는 옵션
+        
+        // TODO: 서버 정지로직 추가
+        Console.WriteLine("서버가 종료되었습니다.");
     }
 }
